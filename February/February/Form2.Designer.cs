29350efc3b20a8e169638635d8eed956c7491017
FrodoZZ <82755564+MrFrodoZZ@users.noreply.github.com>
2023-04-28 20:42:47 +0900

    Add files via upload

---
+++ February/February/Form2.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace February
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(557, 134);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 152);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 297);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(268, 152);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(301, 297);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(576, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(498, 394);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1086, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ February/February/Form2.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace February
+{
+    public partial class Form2 : Form
+    {
+        Button skip = new Button();
+        string[] traits = { "1", "2", "3", "4" };
+
+        
+        public Form2()
+        {
+            InitializeComponent();
+
+            skip.Location = new Point(570, 10);
+            skip.AutoSize = true;
+            skip.Text = "SKIP";
+            skip.Click += new EventHandler(Evhend);
+            skip.Visible = false;
+            Controls.Add(skip);
+
+            DataGridViewColumn[] columns = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn() { Name = "Step", HeaderText = "Шаг" },
+                new DataGridViewTextBoxColumn() { Name = "Choice", HeaderText = "Выбор" }
+            };
+            dataGridView1.Columns.AddRange(columns);
+
+            groupBox1.Text = "";
+        }
+
+        private System.Windows.Forms.Timer timer, skiptimer;
+        private int currentIndex = 0;
+        private string textToDisplay = "ASDLKFJAOSLJKDRSDSAHDKJASHDKJASHDKJASHDKJAHSDKJAHSKDJAHSJKDHAKJSDHKAJSHDKJASHDKJASHDKAJSHDKJAHSDKJASHDKJAHSKJDDSDSDDSDSDSDSDSDSDSDMNFOALIWMDFPOKSFDOPAKJISFG";
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex < textToDisplay.Length)
+            {
+                richTextBox1.AppendText(textToDisplay[currentIndex].ToString());
+                currentIndex++;
+            }
+            else
+            {
+                timer.Stop();
+                skiptimer.Stop();
+                showButtons(4, traits);
+                skip.Visible = false;
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            timer = new System.Windows.Forms.Timer();
+            skiptimer = new System.Windows.Forms.Timer();
+            timer.Interval = 50;
+            skiptimer.Interval = 5000;
+            timer.Tick += Timer_Tick;
+            skiptimer.Tick += Skiptimer_Tick;
+
+            timer.Start();
+            skiptimer.Start();
+
+            ReadFromFile();
+
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            this.BackColor = Color.White;
+
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void Skiptimer_Tick(object sender, EventArgs e)
+        {
+            skip.Visible = true;
+        }
+
+        private void Evhend(object sender, EventArgs e)
+        {
+            switch ((sender as Button).Text)
+            {
+                case "1":
+                    richTextBox1.Text = "абоба";
+                    groupBox1.Controls.Clear();
+                    break;
+                case "2":
+                    richTextBox1.Text = "або";
+                    groupBox1.Controls.Clear();
+                    break;
+                case "3":
+                    richTextBox1.Text = "аб";
+                    groupBox1.Controls.Clear();
+                    break;
+                case "4":
+                    richTextBox1.Text = "а";
+                    groupBox1.Controls.Clear();
+                    break;
+                case "SKIP":
+                    timer.Stop();
+                    skiptimer.Stop();
+                    richTextBox1.Text = textToDisplay;
+                    skip.Visible = false;
+                    showButtons(4, traits);
+                    break;
+            }
+        }
+
+        public void showButtons(int n, string[] t)
+        {
+            Button btn1 = new Button();
+            Button btn2 = new Button();
+            Button btn3 = new Button();
+            Button btn4 = new Button();
+
+            btn1.AutoSize = true;
+            btn2.AutoSize = true;
+            btn3.AutoSize = true;
+            btn4.AutoSize = true;
+
+            btn1.Location = new Point(10, 10);
+            btn2.Location = new Point(10, 50);
+            btn3.Location = new Point(10, 90);
+            btn4.Location = new Point(10, 130);
+
+            btn1.Click += new EventHandler(Evhend);
+            btn2.Click += new EventHandler(Evhend);
+            btn3.Click += new EventHandler(Evhend);
+            btn4.Click += new EventHandler(Evhend);
+
+            btn1.Text = t[0];
+            btn2.Text = t[1];
+            btn3.Text = t[2];
+            btn4.Text = t[3];
+            switch (n)
+            {
+                case 2:
+                    groupBox1.Controls.Add(btn1);
+                    groupBox1.Controls.Add(btn2);
+                    groupBox1.Controls.Remove(btn3);
+                    groupBox1.Controls.Remove(btn4);
+                    break;
+                case 3:
+                    groupBox1.Controls.Add(btn1);
+                    groupBox1.Controls.Add(btn2);
+                    groupBox1.Controls.Add(btn3);
+                    groupBox1.Controls.Remove(btn4);
+                    break;
+                case 4:
+                    groupBox1.Controls.Add(btn1);
+                    groupBox1.Controls.Add(btn2);
+                    groupBox1.Controls.Add(btn3);
+                    groupBox1.Controls.Add(btn4);
+                    break;
+                default:
+                    groupBox1.Controls.Clear();
+                    break;
+            }
+        }
+
+        private void WriteToFile(int s, string c)
+        {
+            using (StreamWriter sw = new StreamWriter("save.txt"))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    int step = Convert.ToInt32(dataGridView1[0, i].Value);
+                    string choice = Convert.ToString(dataGridView1[1, i].Value);
+                    sw.WriteLine(step + " " + choice);
+                }
+                sw.WriteLine(s + " " + c);
+            }
+        }
+
+        private void ReadFromFile()
+        {
+            using (StreamReader sr = new StreamReader("save.txt"))
+            {
+                string data = sr.ReadLine();
+                string[] payload;
+                while (data != "EOF")
+                {
+                    payload = data.Split(' ');
+                    dataGridView1.Rows.Add(payload);
+                    data = sr.ReadLine();
+                }
+            }
+        }
+    }
+}
